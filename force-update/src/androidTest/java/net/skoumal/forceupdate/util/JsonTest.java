344c45e06899235d89980fc9866a9f4e97c381e5
marek_seda <marecek.nenkovice225@gmail.com>
2016-12-16 17:01:51 +0100

    fix tests

--- force-update/src/androidTest/java/net/skoumal/forceupdate/util/JsonTest.java
+++ force-update/src/androidTest/java/net/skoumal/forceupdate/util/JsonTest.java
@@ -4,6 +4,7 @@ import org.json.JSONObject;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 
 /**
@@ -14,11 +15,36 @@ public class JsonTest {
     @Test
     public void normalTest() throws Exception
     {
+
         JSONObject object = new JSONObject("{\n" +
                 "\t\"test01\": \"result01\"\n" +
                 "}");
-
         assertEquals(Json.getStringOrNull(object, "test01"), "result01");
+    }
+    @Test
+    public void returnNullTest() throws Exception
+    {
+        JSONObject object = new JSONObject("{\n" +
+                "\t\"test01\": \"result01\"\n" +
+                "}");
         assertNull(Json.getStringOrNull(object, "test02"));
     }
+    @Test
+    public void nullObjectTest() throws Exception
+    {
+        JSONObject object = null;
+        Json.getStringOrNull(object, "test");
+        fail("Should throw NullPointerException");
+    }
+    @Test
+    public void nestingTest() throws Exception
+    {
+        JSONObject object = new JSONObject("{\n" +
+                "\t\"first\": {\n" +
+                "\t\t\"second\": \"test\"\n" +
+                "\t}\n" +
+                "}");
+        assertEquals("test", Json.getStringOrNull(new JSONObject(Json.getStringOrNull(object, "first")), "second"));
+    }
+
 }
--- force-update/src/androidTest/java/net/skoumal/forceupdate/util/VersionTest.java
+++ force-update/src/androidTest/java/net/skoumal/forceupdate/util/VersionTest.java
@@ -9,7 +9,9 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Created by gingo on 4.12.2016.
@@ -21,15 +23,36 @@ public class VersionTest {
         List<Version> versions = new ArrayList<>();
         Version v1 = new Version("1.0.0");
         Version v2 = new Version("1.1.0");
-        Version v5 = new Version("22.0");
+        Version v3 = new Version("22.0");
         versions.add(v1);
         versions.add(v2);
-        versions.add(v5);
+        versions.add(v3);
         Set<String> set = Versions.toStringSet(versions);
         assertTrue(set.contains("1.0.0"));
         assertTrue(set.contains("1.1.0"));
         assertTrue(set.contains("22.0"));
         assertFalse(set.contains("1.5.1"));
+    }
+
+    @Test
+    public void sizeTest()
+    {
+        List<Version> versions = new ArrayList<>();
+        for(int i = 0; i< 6; i++)
+        {
+            Version v = new Version("1." +i + ".0");
+            versions.add(v);
+        }
+        Set<String> set = Versions.toStringSet(versions);
+        assertSame(set.size(), versions.size());
+    }
+    @Test
+    public void nullTest()
+    {
+        List<Version> versions = new ArrayList<>();
+        versions.add(null);
+        Set<String> set = Versions.toStringSet(versions);
+        fail("Should throw NullPointerException");
     }
 
 }
