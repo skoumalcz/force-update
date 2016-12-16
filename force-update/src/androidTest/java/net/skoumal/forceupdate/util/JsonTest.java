10b7de809a82c8d9775a9d55d553330c451d429a
marek_seda <marecek.nenkovice225@gmail.com>
2016-12-16 14:12:22 +0100

    versionTest, Json test

--- force-update/src/androidTest/java/net/skoumal/forceupdate/util/JsonTest.java
+++ force-update/src/androidTest/java/net/skoumal/forceupdate/util/JsonTest.java
@@ -1,9 +1,24 @@
 package net.skoumal.forceupdate.util;
 
+import org.json.JSONObject;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+
 /**
  * Created by gingo on 4.12.2016.
  */
 public class JsonTest {
 
-    //TODO [1] implement tests
+    @Test
+    public void normalTest() throws Exception
+    {
+        JSONObject object = new JSONObject("{\n" +
+                "\t\"test01\": \"result01\"\n" +
+                "}");
+
+        assertEquals(Json.getStringOrNull(object, "test01"), "result01");
+        assertNull(Json.getStringOrNull(object, "test02"));
+    }
 }
--- force-update/src/androidTest/java/net/skoumal/forceupdate/util/VersionTest.java
+++ force-update/src/androidTest/java/net/skoumal/forceupdate/util/VersionTest.java
@@ -1,9 +1,35 @@
 package net.skoumal.forceupdate.util;
 
+import net.skoumal.forceupdate.Version;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by gingo on 4.12.2016.
  */
 public class VersionTest {
+    @Test
+    public void baseTest()
+    {
+        List<Version> versions = new ArrayList<>();
+        Version v1 = new Version("1.0.0");
+        Version v2 = new Version("1.1.0");
+        Version v5 = new Version("22.0");
+        versions.add(v1);
+        versions.add(v2);
+        versions.add(v5);
+        Set<String> set = Versions.toStringSet(versions);
+        assertTrue(set.contains("1.0.0"));
+        assertTrue(set.contains("1.1.0"));
+        assertTrue(set.contains("22.0"));
+        assertFalse(set.contains("1.5.1"));
+    }
 
-    //TODO [1] implement tests
 }
